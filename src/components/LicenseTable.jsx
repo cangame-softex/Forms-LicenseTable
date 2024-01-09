4408a9729ad69ac893b20f5746648bb206da01b3
italomarcony <italo_marcony4000@hotmail.com>
2024-01-09 19:39:40 -0300

    adicionando header e footer e alterando tabela

--- src/components/LicenseTable.jsx
+++ src/components/LicenseTable.jsx
@@ -2,12 +2,11 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import "../css/LicenseTable.css";
+import { FaInstagram, FaFacebook } from "react-icons/fa";
 
 const LicenseTable = () => {
   const [data, setData] = useState([]);
-  const [totalLicenses, setTotalLicenses] = useState(0);
-  const [licensesInUse, setLicensesInUse] = useState(0);
-  const [licensesAvailable, setLicensesAvailable] = useState(0);
+  const [licenseStats, setLicenseStats] = useState({});
 
   useEffect(() => {
     const fetchData = async () => {
@@ -16,52 +15,75 @@ const LicenseTable = () => {
         const response = await axios.get("https://sua-api.com/licencas");
         setData(response.data);
 
-        // Calcular o número total de licenças
-        setTotalLicenses(response.data.length);
+        const stats = response.data.reduce((acc, license) => {
+          acc[license.id] = {
+            total: acc[license.id]?.total + 1 || 1,
+            inUse:
+              acc[license.id]?.inUse + (license.emUso ? 1 : 0) ||
+              (license.emUso ? 1 : 0),
+          };
+          acc[license.id].available =
+            acc[license.id].total - acc[license.id].inUse;
+          return acc;
+        }, {});
 
-        // Calcular o número de licenças em uso
-        const inUseCount = response.data.filter(
-          (license) => license.emUso
-        ).length;
-        setLicensesInUse(inUseCount);
-
-        // Calcular o número de licenças disponíveis
-        setLicensesAvailable(totalLicenses - inUseCount);
+        setLicenseStats(stats);
       } catch (error) {
         console.error("Erro ao obter dados:", error);
       }
     };
 
     fetchData();
-  }, [totalLicenses]);
+  }, []);
 
   return (
-    <div className="table-container">
-      <div className="table-wrapper">
-        <h2>Controle de Licenças</h2>
-        <p>Total de Licenças: {totalLicenses}</p>
-        <p>Licenças em Uso: {licensesInUse}</p>
-        <p>Licenças Disponíveis: {licensesAvailable}</p>
-        <table className="styled-table">
-          <thead>
-            <tr>
-              <th>ID</th>
-              <th>Nome</th>
-              <th>Tipo</th>
-              <th>Validade</th>
-            </tr>
-          </thead>
-          <tbody>
-            {data.map((license) => (
-              <tr key={license.id}>
-                <td>{license.id}</td>
-                <td>{license.name}</td>
-                <td>{license.type}</td>
-                <td>{license.validade}</td>
+    <div className="app-container">
+      <div className="header" style={{ backgroundColor: "#03588c" }}>
+        <h1>CanGame Edu</h1>
+      </div>
+      <div className="table-container">
+        <div className="table-wrapper">
+          <h2>Controle de Licenças</h2>
+          <table className="styled-table">
+            <thead>
+              <tr>
+                <th>ID</th>
+                <th>Nome</th>
+                <th>Tipo</th>
+                <th>Validade</th>
+                <th>Total</th>
+                <th>Em Uso</th>
+                <th>Disponíveis</th>
               </tr>
-            ))}
-          </tbody>
-        </table>
+            </thead>
+            <tbody>
+              {data.map((license) => (
+                <tr key={license.id}>
+                  <td>{license.id}</td>
+                  <td>{license.name}</td>
+                  <td>{license.type}</td>
+                  <td>{license.validade}</td>
+                  <td>{licenseStats[license.id]?.total || 0}</td>
+                  <td>{licenseStats[license.id]?.inUse || 0}</td>
+                  <td>{licenseStats[license.id]?.available || 0}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
+      <div
+        className="footer"
+        style={{ backgroundColor: "#03588c", marginTop: "auto" }}
+      >
+        <div className="social-icons">
+          <a href="#" target="_blank" rel="noopener noreferrer">
+            <FaInstagram />
+          </a>
+          <a href="#" target="_blank" rel="noopener noreferrer">
+            <FaFacebook />
+          </a>
+        </div>
       </div>
     </div>
   );
